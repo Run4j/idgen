8bf8b145c1c1d77e0ad016bab3b8ccec6bc70528
mahmudsalomov <mahmud.salomov.salomov@gmail.com>
2025-05-27 17:29:20 +0500

    remove test

--- src/test/java/uz/maniac4j/IdGeneratorIT.java
+++
@@ -1,8 +0,0 @@
-package uz.maniac4j;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class IdGeneratorIT extends IdGeneratorTest {
-    // Execute the same tests but in packaged mode.
-}
